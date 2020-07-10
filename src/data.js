4a17c0138f2417fc086437d7d3f13b443e9eb10b
bhoomikhanderia <khanderiabhoomi@gmail.com>
2020-07-10 14:10:51 +1000

    data file restructure

--- src/data.js
+++ src/data.js
@@ -3,16 +3,30 @@ module.exports = [
       sku: 'ipd',
       name: 'Super iPad',
       price: 549.99, 
+      offer: {
+          type: 'bulkPriceDiscount',
+          qty: 4,
+          discountedPrice: 499.99,
+      },
     },
     {
       sku: 'mbp',
       name: 'MacBook Pro',
       price: 1399.99,
+      offer: {
+        type: 'freeItem',
+        freeItemSKU: 'vga',
+      },
     },
     {
       sku: 'atv',
       name: 'Apple TV',
       price: 109.50,
+      offer: {
+        type: 'qtyPriceDiscount',
+        qty: 3,
+        discountQty: 1,
+    },
     },
     {
       sku: 'vga',
